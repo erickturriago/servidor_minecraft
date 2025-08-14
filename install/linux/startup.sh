db3da5fbd108d4e1cba9e4bf20f03386b7425e1a
Erick Turriago <turriago-erick@hotmail.com>
2025-08-14 13:42:32 -0500

    Startup

--- install/linux/startup.sh
+++ install/linux/startup.sh
@@ -7,25 +7,41 @@ INSTALL_DIR="$(dirname "$(realpath "$0")")/../.."
 # ---------------------
 
 # --- FUNCIONES ---
+check_and_install_git() {
+    if ! command -v git &> /dev/null; then
+        echo "--- Git no encontrado. Instalando..."
+        sudo apt-get update
+        sudo apt-get install -y git
+        echo "--- Git instalado."
+    else
+        echo "--- Git ya está instalado."
+    fi
+    
+    # Configurar usuario y correo de Git para los commits
+    git config --global user.email "turriago-erick@hotmail.com"
+    git config --global user.name "Erick Turriago"
+    echo "--- Git configurado con autor."
+}
+
 check_and_install_unzip() {
     if ! command -v unzip &> /dev/null; then
-        echo "--- El programa 'unzip' no esta instalado. Instalando..."
+        echo "--- El programa 'unzip' no está instalado. Instalando..."
         sudo apt-get update
         sudo apt-get install -y unzip
         echo "--- 'unzip' instalado."
     else
-        echo "--- 'unzip' ya esta instalado."
+        echo "--- 'unzip' ya está instalado."
     fi
 }
 
 check_and_install_zip() {
     if ! command -v zip &> /dev/null; then
-        echo "--- El programa 'zip' no esta instalado. Instalando..."
+        echo "--- El programa 'zip' no está instalado. Instalando..."
         sudo apt-get update
         sudo apt-get install -y zip
         echo "--- 'zip' instalado."
     else
-        echo "--- 'zip' ya esta instalado."
+        echo "--- 'zip' ya está instalado."
     fi
 }
 
@@ -37,15 +53,15 @@ check_and_install_docker() {
         sudo mkdir -p /etc/apt/keyrings
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
         echo \
-            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-            $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+          "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+          $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
         sudo apt-get update
         sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
         
         sudo usermod -aG docker "$USER"
         echo "--- Docker instalado. Por favor, cierra la sesion y vuelve a entrar para que los cambios tengan efecto."
     else
-        echo "--- Docker ya esta instalado."
+        echo "--- Docker ya está instalado."
     fi
 
     if ! command -v docker-compose &> /dev/null; then
@@ -56,12 +72,12 @@ check_and_install_docker() {
 
 check_and_install_cron() {
     if ! command -v crontab &> /dev/null; then
-        echo "--- El programa 'cron' no esta instalado. Instalando..."
+        echo "--- El programa 'cron' no está instalado. Instalando..."
         sudo apt-get update
         sudo apt-get install -y cron
         echo "--- 'cron' instalado."
     else
-        echo "--- 'cron' ya esta instalado."
+        echo "--- 'cron' ya está instalado."
     fi
 }
 
@@ -73,7 +89,7 @@ decompress_data() {
     mkdir -p "$INSTALL_DIR/data"
     echo "--- Descomprimiendo la carpeta de datos..."
     unzip -o "$INSTALL_DIR/$COMPRESSED_DATA" -d "$INSTALL_DIR/data"
-    echo "--- Carpeta 'data' descomprimida con exito."
+    echo "--- Carpeta 'data' descomprimida con éxito."
 }
 
 start_and_schedule() {
@@ -85,11 +101,12 @@ start_and_schedule() {
 }
 
 # --- EJECUCIÓN ---
-echo "--- Iniciando el script de instalacion del servidor de Minecraft..."
+echo "--- Iniciando el script de instalación del servidor de Minecraft..."
+check_and_install_git
 check_and_install_unzip
 check_and_install_zip
 check_and_install_docker
 check_and_install_cron
 decompress_data
 start_and_schedule
-echo "--- Configuracion completa! El servidor esta funcionando."
+echo "--- Configuración completa! El servidor está funcionando."
